3af078a17f5607d440a5351ed310b8708ba9d3ca
Lieven Vervoort <lievenvervoort@telenet.be>
2018-01-21 16:32:11 +0100

    some changes

--- app/src/main/java/com/example/lievenvervoort/nieuwsapp/MainActivity.java
+++ app/src/main/java/com/example/lievenvervoort/nieuwsapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.lievenvervoort.nieuwsapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mTitleView;
+    requestHelper url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTitleView = findViewById(R.id.tv_title);
+
+        String data = url.getAll();
+
+
+        mTitleView.setText(data);
+
     }
 }
