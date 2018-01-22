0f957435c3d149e8558e3cdfb95762b2c925df15
Lieven <lieven.vervoort-partner@aperam.com>
2018-01-22 16:02:14 +0100

    changed request

--- app/src/main/java/com/example/lievenvervoort/nieuwsapp/MainActivity.java
+++ app/src/main/java/com/example/lievenvervoort/nieuwsapp/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.lievenvervoort.nieuwsapp;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import org.json.JSONArray;
+import com.android.volley.Header;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import javax.xml.transform.Result;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView mTitleView;
-    requestHelper url;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +30,39 @@ public class MainActivity extends AppCompatActivity {
 
         mTitleView = findViewById(R.id.tv_title);
 
-        String data = url.getAll();
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url="https://developer.nytimes.com/proxy/https/api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=a0324d5f0e924bdb9065345498d16db6";
+
+        JsonObjectRequest jsonObjectRequestRequest = new JsonObjectRequest(Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        mTitleView.setText(response.toString());
+                    }
+                }, new Response.ErrorListener(){
+                    @Override
+                public void onErrorResponse(VolleyError error){
+
+                }
+            }
+        );
 
 
-        mTitleView.setText(data);
 
+        /*Request.Method.GET, url,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        mTitleView.setText(response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTitleView.setText("That didn't work!");
+            }
+        }*/
+        queue.add(jsonObjectRequestRequest);
     }
 }
---
+++ app/src/main/java/com/example/lievenvervoort/nieuwsapp/VolleyHelper.java
@@ -0,0 +1,9 @@
+package com.example.lievenvervoort.nieuwsapp;
+
+/**
+ * Created by LievenVervoort on 22/01/2018.
+ */
+
+public class VolleyHelper {
+
+}
